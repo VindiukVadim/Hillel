4a83ac77919e9837ef50ab13ddf8cb0a50d8f556
Vadym <agentvinjukv1@gmail.com>
2023-03-09 23:38:23 +0100

    Add calculating the sum of numbers

--- SumAllNumbers1_100/SumAllNumbers1_100/Program.cs
+++ SumAllNumbers1_100/SumAllNumbers1_100/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
+            int sum=0;
+            Console.Clear();
             Console.WriteLine("Hello, World!");
+            for(var i=1; i<=100; i++) 
+            {
+                 sum += i;
+            }
+            Console.WriteLine("sum of all numbers with 1 to 100 = " + sum);
+            Console.ReadLine();
         }
     }
 }
