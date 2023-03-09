5d816735aaa1e2297db0db476e4156165f04e5d5
Vadym <agentvinjukv1@gmail.com>
2023-03-09 23:18:20 +0100

    AddConsoleApp

---
+++ SumAllNumbers1_100/SumAllNumbers1_100/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SumAllNumbers1_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
