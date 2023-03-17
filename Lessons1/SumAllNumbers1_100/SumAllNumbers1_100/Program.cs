49cdf8f882b8135b3b4124f4083a2a689ac9613e
Vadym <agentvinjukv1@gmail.com>
2023-03-17 22:17:55 +0100

    add Lessons1

---
+++ Lessons1/SumAllNumbers1_100/SumAllNumbers1_100/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SumAllNumbers1_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            Console.Clear();
+            Console.WriteLine("Lektion #1. Learning GIT");
+            for(var i=1; i<=100; i++) 
+            {
+                 sum += i;
+            }
+            Console.WriteLine("sum of all numbers with 1 to 100 = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
