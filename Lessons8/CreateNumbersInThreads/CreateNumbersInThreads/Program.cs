7a7cd9525612d5b7abfd186e18729b087821c3c5
Vadym <agentvinjukv1@gmail.com>
2023-04-05 09:40:37 +0200

    Finaly Generator

---
+++ Lessons8/CreateNumbersInThreads/CreateNumbersInThreads/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace CreateNumbersInThreads
+{
+    internal class Program
+    {
+        static int evenCounter = 0;
+        static int notEvenCounter = 0;
+        static readonly object lockObject = new object();
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(GenerateRandomNumbers));
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine($"even numbers: {evenCounter}");
+            Console.WriteLine($"not even numbers: {notEvenCounter}");
+        }
+
+        static void GenerateRandomNumbers()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int randomNumber = rnd.Next(1, 101);
+
+                lock (lockObject)
+                {
+                    if (randomNumber % 2 == 0)
+                    {
+                        evenCounter++;
+                    }
+                    else
+                    {
+                        notEvenCounter++;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
