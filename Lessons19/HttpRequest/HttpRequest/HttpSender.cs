eb3ddb72298a61e01a88ddf9526b04bbae5778e5
Vadym <agentvinjukv1@gmail.com>
2023-05-26 20:15:40 +0200

    Httprequest

---
+++ Lessons19/HttpRequest/HttpRequest/HttpSender.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HttpRequest
+{
+    public class HttpSender
+    {
+        public async void GetRequest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var url = "https://example.com";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response:");
+                Console.WriteLine(responseBody);
+            }
+        }
+
+        public async void PostRequest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var url = "https://example.com";
+                HttpResponseMessage response = await client.PostAsync(url, new StringContent("example"));
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response:");
+                Console.WriteLine(responseBody);
+            }
+        }
+
+        public async void PutRequest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var url = "https://example.com";
+                HttpResponseMessage response = await client.PutAsync(url, new StringContent("example"));
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response:");
+                Console.WriteLine(responseBody);
+            }
+        }
+
+        public async void DeleteRequest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var url = "https://example.com";
+                HttpResponseMessage response = await client.DeleteAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response:");
+                Console.WriteLine(responseBody);
+            }
+        }
+    }
+}
---
+++ Lessons19/HttpRequest/HttpRequest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace HttpRequest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+        }
+    }
+}
