5c3450b74e85062544245f52fac733cdc3ef7cb5
Vadym <agentvinjukv1@gmail.com>
2023-05-26 19:25:41 +0200

    fix

--- Lessons18/Main.js
+++ Lessons18/Main.js
@@ -17,6 +17,43 @@ document.addEventListener("DOMContentLoaded", (event) => {
         var third = document.getElementById('third');
         third.classList.toggle('hide');
     });
+
+    const form = document.getElementById('SendForm');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+        validateForm()
+           
+    });
+
+    function validateForm() {
+        const firstName = document.getElementById('Name').value;
+        const secondName = document.getElementById('SecondName').value;
+        const email = document.getElementById('Email').value;
+        const phone = document.getElementById('Phone').value;
+        const form = document.getElementById('SendForm');
+
+        if (firstName.trim() === '' || secondName.trim() === '' || email.trim() === '' || phone.trim() === '') {
+            
+            form.style.border = "1px solid red";
+            form.style.boxShadow = "0px 0px 10px red";
+            alert("Данні в формі не відповідають правилам!");
+        }
+        else {
+            form.style.border = "1px solid green";
+            form.style.boxShadow = "0px 0px 10px green";
+            clearForm();
+            alert("Форма відправлена!")
+        }
+    }
+
+    function clearForm() {
+        document.getElementById('Name').value = '';
+        document.getElementById('SecondName').value = '';
+        document.getElementById('Email').value = '';
+        document.getElementById('Phone').value = '';
+    }
 });
+
 
 
